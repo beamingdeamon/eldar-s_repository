787c333104029c0a3ab11b9f05f41657c5a931e8
beamingdeamon <eldar_2301@mail.ru>
2020-03-05 21:41:52 +0600

    Js

---
+++ script.js
@@ -0,0 +1,145 @@
+'use strict'
+
+window.onload = function(){
+    let
+        modal = document.querySelector('#modal-body'),
+        modal_close = document.querySelector('#modal-close'),
+        right_arrow = document.querySelector('.right-arrow > span'),
+        left_arrow = document.querySelector('.left-arrow > span'),
+        avatar = document.querySelector('#modal-avatar'),
+        description = document.querySelector('#modal-description'),
+        page_index = 1,
+        max_pages = 0,
+        loaded = 0,
+        oneChange = 0,
+        ArrayLoad = []
+    ;
+    
+    loadData(page_index, 'default');
+
+    function loadData(page, road){
+        fetch('https://rickandmortyapi.com/api/character/?page=' + page)
+            .then(response => {
+                console.log(response);
+                if(response.status !== 200){
+                    console.log('sorry, error...');
+                } else {
+                    clearList();
+                    return response.json();
+                }
+            })
+            .then(json => renderList(json, road))
+            .catch(err => console.log(err));
+    }
+    
+    function renderList(data, road){
+        let 
+            rawData = data,
+            list = rawData.results,
+            info = rawData.info
+        ;
+        console.log(list);
+
+        if(oneChange < 1)
+        {
+            for(let i = 1; i <= info.pages; i++){
+                ArrayLoad[i] = i;
+            }
+            oneChange++;
+        }
+
+
+        if(inArray(page_index, ArrayLoad) + 1){
+            if(loaded <= info.count && road != 'left')
+            {
+                loaded += list.length;
+                delete ArrayLoad[inArray(page_index, ArrayLoad)];
+                console.log(ArrayLoad);
+            }
+        }
+
+
+        
+        max_pages = info.pages;
+
+        document.querySelector('.allCurrent').textContent = info.count;
+        document.querySelector('.current').textContent = loaded;
+        document.querySelector('.max-pages').textContent = info.pages;
+        document.querySelector('.current-pages').textContent = page_index;
+
+
+        const listContainer = document.querySelector('.relative_front_sec1');
+
+        list.forEach((el, i, arr) => {
+
+            listContainer.innerHTML += `
+                <li id="item">${el.id} &nbsp;&nbsp;&nbsp; ${el.name}</li>
+            `
+        })
+
+        let actionList = document.querySelectorAll('#item');
+
+        for (let i = 0; i < actionList.length; i++) {
+            actionList[i].addEventListener('click', function(){
+                avatar.style.cssText = `
+                    background: url('${list[i].image}') center no-repeat;
+                    background-size: cover;
+                `
+                description.innerHTML = `
+                    <li>Name: ${list[i].name}</li>
+                    <li>Status: ${list[i].status}</li>
+                    <li>Species: ${list[i].species}</li>
+                    <li>Gender: ${list[i].gender}</li>
+                `
+                
+
+                modal.classList.remove('display_none');
+                modal.classList.add('display_flex');
+                modal_close.classList.remove('display_none');
+            })
+        }
+
+    }
+
+    function inArray(checker, array){
+        for (let i = 0; i < array.length; i++)
+        {
+            if(array[i] == checker)
+                return i;
+        }
+    }
+
+    function clearList(){
+        let actionList = document.querySelectorAll('#item');
+
+        for (let i = 0; i < actionList.length; i++)
+            actionList[i].remove();
+    }
+
+    right_arrow.addEventListener('click', function(){
+        if(page_index + 1 <= max_pages)
+        {
+            page_index++;
+            loadData(page_index, 'right');
+            
+        }
+    })
+
+    left_arrow.addEventListener('click', function(){
+        if(page_index - 1 > 0)
+        {
+            page_index--;
+            
+            loadData(page_index, 'left');
+        }
+    })
+
+    
+    
+
+    modal_close.addEventListener('click', function(){
+        modal_close.classList.add('display_none');
+        modal.classList.remove('display_flex');
+        modal.classList.add('display_none');
+    })
+}
